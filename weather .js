54e4e60562e9d9e78544118eef9fce3f695f6368
manu1242 <manoharyalla77@gmail.com>
2025-02-20 13:47:26 +0530

    changes in function

--- weather .js
+++ weather .js
@@ -9,40 +9,47 @@ let clouds = document.getElementById('clouds');
 let humidity = document.getElementById('humidity');
 let pressure = document.getElementById('pressure');
 let main = document.querySelector('main');
+let searchBtn = document.getElementById('search-btn');
+
 form.addEventListener("submit", (e) => {
-    e.preventDefault();  
-    if(valueSearch.value != ''){
+    e.preventDefault();
+    if (valueSearch.value.trim() !== '') {
         searchWeather();
     }
 });
+
+searchBtn.addEventListener("click", () => {
+    if (valueSearch.value.trim() !== '') {
+        searchWeather();
+    }
+});
+
 const searchWeather = () => {
-    fetch(url+'&q='+ valueSearch.value)
+    fetch(url + '&q=' + valueSearch.value)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-            if(data.cod == 200){
+            if (data.cod == 200) {
                 city.querySelector('figcaption').innerHTML = data.name;
                 city.querySelector('img').src = `https://flagsapi.com/${data.sys.country}/shiny/32.png`;
                 temperature.querySelector('img').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
                 temperature.querySelector('span').innerText = data.main.temp;
                 description.innerText = data.weather[0].description;
-
                 clouds.innerText = data.clouds.all;
                 humidity.innerText = data.main.humidity;
                 pressure.innerText = data.main.pressure;
-            }else{
+            } else {
                 main.classList.add('error');
                 setTimeout(() => {
                     main.classList.remove('error');
                 }, 1000);
             }
             valueSearch.value = '';
-        })
-}
-// search Default
+        });
+};
+
 const initApp = () => {
     valueSearch.value = 'Bhimavaram';
     searchWeather();
-}
-initApp();
+};
 
+initApp();
